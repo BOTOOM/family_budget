e816d1fdc6ed532657fe07a714a80fdfd50960b3
BOTOOM <edwardiaz.dev@gmail.com>
2024-08-08 15:15:08 -0500

    eliminacion de colores negros #12

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
@@ -5,7 +6,7 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
@@ -17,9 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+    <html lang="es" className={GeistSans.className}>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300">
+        <main className="">
+        {/* <main className="min-h-screen flex flex-col items-center"> */}
           {children}
         </main>
       </body>
